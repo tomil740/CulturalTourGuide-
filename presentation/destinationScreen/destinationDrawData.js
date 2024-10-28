cba91b46017a53b32bcd6e056f9079794e2cc7bf
jvseds <jvaaltonen@gmail.com>
2024-10-28 19:11:00 +0200

    modified destination.html structure to receive data from json using destination.js, connected to js

--- presentation/destinationScreen/destinationDrawData.js
+++ presentation/destinationScreen/destinationDrawData.js
@@ -1,12 +1,10 @@
-class DestinationDrawData{
-
-    constructor(destinationObj){
-        document.querySelector("main");
-        const ele = document.createElement("div");
-        ele.innerHTML = `<h3>working with the obj ${destinationObj}</h3>`;
-    }
-
-
+class DestinationDrawData {
+  constructor(destinationObj) {
+    const mainContainer = document.querySelector("#destinationContent");
+    const ele = document.createElement("div");
+    ele.innerHTML = `<h3>working with the obj ${destinationObj}</h3>`;
+    mainContainer.appendChild(ele);
+  }
 }
 
 export default DestinationDrawData;
