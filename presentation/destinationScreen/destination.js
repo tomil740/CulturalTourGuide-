9edef32a0451fccd5f5e24382821e204ccced8a4
jvseds <jvaaltonen@gmail.com>
2024-10-29 12:13:48 +0200

    rendered json data in destination page html

--- presentation/destinationScreen/destination.js
+++ presentation/destinationScreen/destination.js
@@ -6,11 +6,21 @@ class Destination {
   constructor() {
     console.log("running");
 
-    // get the curren destination
+    // get the current destination
     const currentDest = repository.getCurrentDestination();
     this.#drawObj = new destinationDrawData(currentDest);
   }
 }
 
-const destination = new Destination();
+const mockDestination = {
+  _id: "123",
+  city: "Test City",
+  population: 1000000,
+  imgUrls: ["https://example.com/image.jpg"],
+  summary: "This is a test city used for testing purposes.",
+  category: ["testCategory1", "testCategory2"],
+  loc: { lat: 40.7128, lng: -74.006 },
+};
+
+const destination = new Destination(mockDestination);
 destination;
--- presentation/destinationScreen/destinationDrawData.js
+++ presentation/destinationScreen/destinationDrawData.js
@@ -7,7 +7,7 @@ class DestinationDrawData {
 
     // initial div to make sure everything is correct
     const ele = document.createElement("div");
-    ele.innerHTML = `<h3>working with the obj ${destinationObj["city"]}</h3>`;
+    ele.innerHTML = `<h3>working with the obj ${this.destinationObj["city"]}</h3>`;
     mainContainer.appendChild(ele);
 
     // append the other data elements
@@ -20,7 +20,7 @@ class DestinationDrawData {
   // methods to create elements from data drawn from JSON
   createDataEl(mainContainer) {
     Object.keys(this.destinationObj).forEach((key) => {
-      let val = destinationObj[key];
+      let val = this.destinationObj[key];
 
       // append value only if it's not a nested value
       if (!Array.isArray(val)) {
@@ -47,7 +47,7 @@ class DestinationDrawData {
   gameButtonText() {
     // get game activation button
     const gameBtn = document.querySelector("#activateGame");
-    gameBtn.textContent = `show us what you know about ${destinationObj[city]}!`;
+    gameBtn.textContent = `show us what you know about ${this.destinationObj[city]}!`;
   }
 }
 
