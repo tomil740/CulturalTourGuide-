d321984ab7aa8d5da165145ee4a0aa7eab1896aa
jvseds <jvaaltonen@gmail.com>
2024-10-29 08:59:07 +0200

    added id attribute to element div

--- presentation/destinationScreen/destinationDrawData.js
+++ presentation/destinationScreen/destinationDrawData.js
@@ -5,16 +5,23 @@ class DestinationDrawData {
     ele.innerHTML = `<h3>working with the obj ${destinationObj}</h3>`;
     mainContainer.appendChild(ele);
   }
+
   // methods to create elements from data drawn from JSON
   createDataElement(key, val) {
     const dataElementDiv = document.createElement("div");
+    // set the div id to the corresponding key
+    dataElementDiv.setAttribute("id", key);
+
     const hElement = document.createElement("h3");
     const dataContent = document.createElement("div");
+
     hElement.textContent = `${key}`;
     dataContent.textContent = `${val}`;
+
     // append h3 and content to the div
     dataElementDiv.appendChild(hElement);
     dataElementDiv.appendChild(dataContent);
+
     // append div to main element
     mainContainer.appendChild(dataElementDiv);
   }
