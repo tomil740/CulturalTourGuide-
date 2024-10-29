cfeaba00ff41dc5cee9f8b4737bec903a3c79c42
jvseds <jvaaltonen@gmail.com>
2024-10-29 07:52:08 +0200

    method to append data elements

--- presentation/destinationScreen/destinationDrawData.js
+++ presentation/destinationScreen/destinationDrawData.js
@@ -6,11 +6,18 @@ class DestinationDrawData {
     mainContainer.appendChild(ele);
   }
   // methods to create elements from data drawn from JSON
-  createCityNameEl() {}
-  createDescriptionEl() {}
-  createCulturalSitesEl() {}
-  createReligiousSitesEl() {}
-  createImageGalleryEl() {}
+  createDataElement(key, val) {
+    const dataElementDiv = document.createElement("div");
+    const hElement = document.createElement("h3");
+    const dataContent = document.createElement("div");
+    hElement.textContent = `${key}`;
+    dataContent.textContent = `${val}`;
+    // append h3 and content to the div
+    dataElementDiv.appendChild(hElement);
+    dataElementDiv.appendChild(dataContent);
+    // append div to main element
+    mainContainer.appendChild(dataElementDiv);
+  }
 }
 
 export default DestinationDrawData;
