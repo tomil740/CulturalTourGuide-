36935a4c7db8f696b929862f7b1d45d1fa1e0f36
jvseds <jvaaltonen@gmail.com>
2024-10-29 11:00:08 +0200

    changed destination data element editing function

--- presentation/destinationScreen/destination.js
+++ presentation/destinationScreen/destination.js
@@ -9,5 +9,18 @@ class Destination {
     this.#drawObj = new destinationDrawData(a);
   }
 }
-const destination = new Destination();
+
+const mockObject = `{
+  "_id": "123",
+  "city": "Jerusalem",
+  "population": 8000000,
+  "imgUrls": ["https://t4.ftcdn.net/jpg/06/01/54/75/360_F_601547536_E7ovUzbSUOCnoodB9At0wWnyzpUlgnwf.jpg"],
+  "summary": "test test",
+  "category": ["rests", "hotels"],
+  "loc": {
+    "lat": -156.6917,
+    "lan": 20.93792
+  }`;
+
+const destination = new Destination(mockObject);
 destination;
--- presentation/destinationScreen/destinationDrawData.js
+++ presentation/destinationScreen/destinationDrawData.js
@@ -1,5 +1,17 @@
 class DestinationDrawData {
   constructor(destinationObj) {
+    const mockObject = `{
+      "_id": "123",
+      "city": "Jerusalem",
+      "population": 8000000,
+      "imgUrls": ["https://t4.ftcdn.net/jpg/06/01/54/75/360_F_601547536_E7ovUzbSUOCnoodB9At0wWnyzpUlgnwf.jpg"],
+      "summary": "test test",
+      "category": ["rests", "hotels"],
+      "loc": {
+        "lat": -156.6917,
+        "lan": 20.93792
+      }`;
+
     const mainContainer = document.querySelector("#destinationContent");
     const ele = document.createElement("div");
     ele.innerHTML = `<h3>working with the obj ${destinationObj}</h3>`;
@@ -7,23 +19,29 @@ class DestinationDrawData {
   }
 
   // methods to create elements from data drawn from JSON
-  createDataElement(key, val) {
-    const dataElementDiv = document.createElement("div");
-    // set the div id to the corresponding key
-    dataElementDiv.setAttribute("id", key);
+  createDataEl() {
+    Object.keys(destinationObj).forEach((key) => {
+      let val = destinationObj.key;
+      // append value only if it's not a nested value
+      if (!Array.isArray(val)) {
+        const dataElementDiv = document.createElement("div");
+        // set the div id to the corresponding key
+        dataElementDiv.setAttribute("id", key);
 
-    const hElement = document.createElement("h3");
-    const dataContent = document.createElement("div");
+        const hElement = document.createElement("h3");
+        const dataContent = document.createElement("div");
 
-    hElement.textContent = `${key}`;
-    dataContent.textContent = `${val}`;
+        hElement.textContent = `${key}`;
+        dataContent.textContent = `${val}`;
 
-    // append h3 and content to the div
-    dataElementDiv.appendChild(hElement);
-    dataElementDiv.appendChild(dataContent);
+        // append h3 and content to the div
+        dataElementDiv.appendChild(hElement);
+        dataElementDiv.appendChild(dataContent);
 
-    // append div to main element
-    mainContainer.appendChild(dataElementDiv);
+        // append div to main element
+        mainContainer.appendChild(dataElementDiv);
+      }
+    });
   }
 }
 
