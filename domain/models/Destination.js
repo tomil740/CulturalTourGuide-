1960c88306bd95c9a4ecb2411c3b130e08adb578
israel litvak <lisrael1989@gmail.com>
2024-10-30 14:08:06 +0200

    Update the city description cards ui

--- domain/models/Destination.js
+++ domain/models/Destination.js
@@ -3,6 +3,7 @@ class Destination {
     this.id = argObj.id;
     this.city = argObj.city;
     this.image = argObj.imgUrls;
+    this.population = argObj.population;
     this.description = argObj.summary;
     this.restaurants = argObj.category[0];
     this.attractions = argObj.category[1];
